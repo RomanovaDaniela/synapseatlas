168b1d9b162eea6fcf8eb8709ce39963eda7892d
RomanovaDaniela <romanova.daniela.2019@gmail.com>
2026-03-04 13:37:20 +0100

    Touch up synapseatlas/utils/amrdkown.py (no functional change)

--- synapseatlas/utils/markdown.py
+++ synapseatlas/utils/markdown.py
@@ -3,3 +3,5 @@ HDR = re.compile(r"^(#{1,6})\s+(.*)$", re.MULTILINE)
 def headings(md: str): return [(m.group(1), m.group(2).strip()) for m in HDR.finditer(md or "")]
 
 # autosave 2025-10-06T10:48:06.382817+00:00
+
+# autosave 2026-03-04T12:37:20.610353+00:00
