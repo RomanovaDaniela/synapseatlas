2079f6b8e0c923a2985b5f473d99ae9de9772b72
RomanovaDaniela <romanova.daniela.2019@gmail.com>
2026-05-08 13:16:08 +0200

    Quiet but helpful refactor

--- synapseatlas/bookmarks/store.py
+++ synapseatlas/bookmarks/store.py
@@ -27,3 +27,4 @@ def add(url: str, title: Optional[str]=None, tags: Optional[List[str]]=None, not
 # autosave 2026-01-21T18:32:19.841617+00:00
 # tweak 2026-03-20T13:15:23.429567+00:00
 # tweak 2026-04-15T15:56:29.475364+00:00
+# tweak 2026-05-08T11:16:08.154130+00:00
