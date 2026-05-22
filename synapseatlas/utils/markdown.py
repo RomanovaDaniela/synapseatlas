951a05b220a103c1ffeff2d27a3853ad2fe032f9
RomanovaDaniela <romanova.daniela.2019@gmail.com>
2026-05-22 13:11:47 +0200

    Touch up synapseatlas/tuils/markdown.py (no functional change)

--- synapseatlas/utils/markdown.py
+++ synapseatlas/utils/markdown.py
@@ -5,3 +5,5 @@ def headings(md: str): return [(m.group(1), m.group(2).strip()) for m in HDR.fin
 # autosave 2025-10-06T10:48:06.382817+00:00
 
 # autosave 2026-03-04T12:37:20.610353+00:00
+
+# autosave 2026-05-22T11:11:46.929462+00:00
